b878d8ae1998100ac80ebc84302feb469b1c58e4
Supzero-0 <mat.lecanu@gmail.com>
2022-12-13 15:36:14 +0100

    Creation of database, entity Idea + Commentary, make crud of idea + commentary

--- src/Entity/Idea.php
+++ src/Entity/Idea.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\IdeaRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -22,6 +24,14 @@ class Idea
 
     #[ORM\Column(length: 255)]
     private ?string $status = null;
+
+    #[ORM\OneToMany(mappedBy: 'idea', targetEntity: Commentary::class, orphanRemoval: true)]
+    private Collection $commentaries;
+
+    public function __construct()
+    {
+        $this->commentaries = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -60,6 +70,36 @@ class Idea
     public function setStatus(string $status): self
     {
         $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Commentary>
+     */
+    public function getCommentaries(): Collection
+    {
+        return $this->commentaries;
+    }
+
+    public function addCommentary(Commentary $commentary): self
+    {
+        if (!$this->commentaries->contains($commentary)) {
+            $this->commentaries->add($commentary);
+            $commentary->setIdea($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCommentary(Commentary $commentary): self
+    {
+        if ($this->commentaries->removeElement($commentary)) {
+            // set the owning side to null (unless already changed)
+            if ($commentary->getIdea() === $this) {
+                $commentary->setIdea(null);
+            }
+        }
 
         return $this;
     }
--- src/Form/CommentaryType.php
+++ src/Form/CommentaryType.php
@@ -2,27 +2,29 @@
 
 namespace App\Form;
 
-use App\Entity\Idea;
+use App\Entity\Commentary;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
-class IdeaType extends AbstractType
+class CommentaryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title', TextType::class)
+            ->add('author', TextType::class)
             ->add('content', TextareaType::class)
-            ->add('status', TextType::class);
+            ->add('date', DateTimeType::class)
+            ->add('idea', null, ['choice_label' => 'name']);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Idea::class,
+            'data_class' => Commentary::class,
         ]);
     }
 }
