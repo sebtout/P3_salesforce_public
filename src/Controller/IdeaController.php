167af00de38968bd7a02193820438cb5ab169bb6
MOHAMED Ibrahim <mohamed.ibm@hotmail.fr>
2022-12-14 09:27:04 +0100

    resolve quality code

--- src/Controller/IdeaController.php
+++ src/Controller/IdeaController.php
@@ -69,7 +69,7 @@ class IdeaController extends AbstractController
     #[Route('/{id}', name: 'app_idea_delete', methods: ['POST'])]
     public function delete(Request $request, Idea $idea, IdeaRepository $ideaRepository): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$idea->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $idea->getId(), $request->request->get('_token'))) {
             $ideaRepository->remove($idea, true);
         }
 
