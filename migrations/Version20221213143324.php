44579ded3dec0344651aba30706c17874ad3e462
emainguelag <110185005+emainguelag@users.noreply.github.com>
2022-12-14 12:35:25 +0100

    show idea page - comments section unfinished

---
+++ migrations/Version20221213143324.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221213143324 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE idea ADD status VARCHAR(255) NOT NULL, DROP author, DROP picture, DROP date, CHANGE title title VARCHAR(255) NOT NULL, CHANGE content content LONGTEXT NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE messenger_messages');
+        $this->addSql('ALTER TABLE idea ADD author VARCHAR(250) NOT NULL, ADD picture VARCHAR(250) NOT NULL, ADD date DATETIME NOT NULL, DROP status, CHANGE title title VARCHAR(250) NOT NULL, CHANGE content content TEXT DEFAULT NULL');
+    }
+}
