131e1f30a27fdb675377ae5c7094f66b490ff259
emainguelag <110185005+emainguelag@users.noreply.github.com>
2022-12-15 12:23:54 +0100

    Merge branch 'dev'

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -6,17 +6,18 @@ use App\Entity\Comment;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
 class CommentType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('author')
-            ->add('content')
-            ->add('createdAt')
-            ->add('idea', null, ['choice_label' => 'title'])
-        ;
+            ->add('author', TextType::class)
+            ->add('content', TextareaType::class)
+            ->add('idea', null, ['choice_label' => 'title']);
     }
 
 
