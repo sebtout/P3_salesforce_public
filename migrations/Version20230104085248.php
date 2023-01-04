e4819f7d06f60fe54da6501b1b75c60712fb7912
Supzero-0 <mat.lecanu@gmail.com>
2023-01-04 11:19:25 +0100

    add assert for comment entity + correction of errors

--- migrations/Version20230104085248.php
+++ migrations/Version20230104085248.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230103151433 extends AbstractMigration
+final class Version20230104085248 extends AbstractMigration
 {
     public function getDescription(): string
     {
