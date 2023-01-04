b6a0bb861f86b278d0e3627ce72c943cd510d581
emainguelag <110185005+emainguelag@users.noreply.github.com>
2023-01-04 16:24:57 +0100

    migrations corrected

--- migrations/Version20230104152200.php
+++ migrations/Version20230104152200.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230104133939 extends AbstractMigration
+final class Version20230104152200 extends AbstractMigration
 {
     public function getDescription(): string
     {
