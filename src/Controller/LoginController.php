ba88860256b5a57546f57a1439efc262fd471361
MOHAMED Ibrahim <mohamed.ibm@hotmail.fr>
2023-01-04 17:13:38 +0100

    add restriction page and little modification

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Controller\Exception;
 
 class LoginController extends AbstractController
 {
@@ -28,7 +29,5 @@ class LoginController extends AbstractController
     #[Route('/logout', name: 'app_logout', methods: ['GET'])]
     public function logout(): void
     {
-        // controller can be blank: it will never be called!
-        //throw new \Exception('Don\'t forget to activate logout in security.yaml');
     }
 }
