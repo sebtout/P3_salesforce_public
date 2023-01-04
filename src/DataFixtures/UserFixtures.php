d07f3d7c19015782b237453313b23bc3bed540c0
emainguelag <110185005+emainguelag@users.noreply.github.com>
2023-01-04 10:36:55 +0100

    user created

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+
+    public function load(ObjectManager $manager): void
+    {
+        // Création d’un utilisateur de type “contributeur” (= auteur)
+        $user = new User();
+        $user->setEmail('user@salesforce.com');
+        $user->setRoles(['ROLE_USER']);
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $user,
+            '123s@lesforce'
+        );
+
+        $user->setPassword($hashedPassword);
+        $manager->persist($user);
+
+        // Création d’un utilisateur de type “administrateur”
+        $admin = new User();
+        $admin->setEmail('admin@salesforce.com');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $admin,
+            '123s@lesforce'
+        );
+        $admin->setPassword($hashedPassword);
+        $manager->persist($admin);
+
+        // Sauvegarde des 2 nouveaux utilisateurs :
+        $manager->flush();
+    }
+}
